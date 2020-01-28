dd69da07d394ad26389348d28aec5b089009df89
eugene_targonski <tevstim@gmail.com>
2020-01-28 18:04:10 +0300

    Add initial user model

---
+++ XTestBackEnd/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XTestBackEnd.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string GoogleID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string EMail { get; set; }
+        public string Description { get; set; }
+
+    }
+}
