8983e271778e8696586771bc4776dce2b8d3e866
eugene_targonski <tevstim@gmail.com>
2020-02-28 17:00:32 +0300

    no changes

--- Contracts/Models/WorkingTime.cs
+++ Contracts/Models/WorkingTime.cs
@@ -12,12 +12,6 @@ namespace Contracts.Models
         public DateTime WorkTimeEnd { get; set; }
         public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
-        public bool IsMondayOff { get; set; }
-        public bool IsTuesdayOff { get; set; }
-        public bool IsWednesdayOff { get; set; }
-        public bool IsThursdayOff { get; set; }
-        public bool IsFridayOff { get; set; }
-        public bool IsSaturdayOff { get; set; }
-        public bool IsSundayOff { get; set; }
+        public byte WeeklyHolydays { get; set; }
     }
 }
